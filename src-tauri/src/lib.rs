ba91dfbb8fc54d27fa78b026dc900b61af585df5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-18 19:51:54 +0200

    feat: read standard notation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,6 +29,17 @@ fn deserialize_connections(connections: [(u8, u8); 4]) -> [u8; 4] {
     connections.map(|connection| deserialize_connection(connection))
 }
 
+fn parse_standard_notation(notation: &str) -> [(u8, u8); 4] {
+    let mut connections = [(0, 0); 4];
+    let mut i = 0;
+    for connection in notation.split('-') {
+        let (from, to) = connection.split_at(1);
+        connections[i] = (from.parse::<u8>().unwrap() - 1, to.parse::<u8>().unwrap() - 1);
+        i += 1;
+    }
+    connections
+}
+
 // rotate the points on a tile by 90 degrees
 fn rotate_points(points: u8) -> u8 {
     points << 2 | points >> 6
@@ -74,11 +85,56 @@ fn test() -> [(u8, u8); 4] {
     serialize_connections(tile.connections)
 }
 
+#[tauri::command]
+fn get_possible_connections() -> Vec<[(u8, u8); 4]> {
+    let possible_connections = [
+        "12-34-56-78",
+        "14-27-36-58",
+        "15-26-37-48",
+        "16-25-38-47",
+        "18-23-45-67",
+        "12-37-48-56",
+        "12-38-47-56",
+        "16-25-37-48",
+        "17-24-35-68",
+        "15-27-36-48",
+        "17-28-35-46",
+        "18-26-37-45",
+        "18-27-36-45",
+        "13-26-48-57",
+        "15-28-37-46",
+        "12-35-47-68",
+        "12-36-47-58",
+        "12-38-45-67",
+        "12-38-46-57",
+        "17-24-36-58",
+        "18-23-46-57",
+        "12-34-57-68",
+        "12-34-58-67",
+        "16-23-47-58",
+        "16-28-35-47",
+        "17-23-46-58",
+        "17-28-36-45",
+        "12-36-48-57",
+        "12-37-46-58",
+        "12-37-45-68",
+        "12-37-45-68",
+        "12-37-45-68",
+        "15-28-36-47",
+        "13-25-48-67",
+        "16-28-37-45",
+    ]
+        .map(|it| parse_standard_notation(it))
+        .map(|it| deserialize_connections(it));
+
+    possible_connections.map(|it| serialize_connections(it)).to_vec()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![test])
+        .invoke_handler(tauri::generate_handler![test, get_possible_connections])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
