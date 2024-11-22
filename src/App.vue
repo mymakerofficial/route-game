2dac7761a2700e5675fe18cd180a68626ef4a0ff
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-11-22 17:37:11 +0100

    feat: ui better

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,14 @@ import {useGetTileStack} from "@/composables/queries/useGetTileStack.ts";
 import {useGetGameBoard} from "@/composables/queries/useGetGameBoard.ts";
 import {useResetGame} from "@/composables/mutations/useResetGame.ts";
 import {useToggle} from "@vueuse/core";
+import {InfoIcon, UndoIcon, EyeIcon, ArrowUpFromLineIcon, RotateCcwIcon, SquareAsteriskIcon} from "lucide-vue-next";
 
 const { data: stack } = useGetTileStack()
 const { data: board } = useGetGameBoard()
 const { data: players } = useGetPlayers()
 const { mutateAsync: resetGame } = useResetGame()
 
-const [showTileStack, toggleTileStack] = useToggle()
+const [showNerdStuff, toggleNerdStuff] = useToggle()
 </script>
 
 <template>
@@ -23,7 +24,7 @@ const [showTileStack, toggleTileStack] = useToggle()
       <div>
         <GameGrid :tiles="board" :players="players" class="max-w-screen-md" />
       </div>
-      <div class="flex flex-col gap-12">
+      <div class="flex flex-col gap-6">
         <div class="flex flex-wrap gap-4">
           <div v-for="(player, index) in players" :key="index" class="space-y-2">
             <h3
@@ -42,18 +43,36 @@ const [showTileStack, toggleTileStack] = useToggle()
             <PlayerTileStack :tiles="player.tileStack" :player-index="index" />
           </div>
         </div>
-        <div class="flex gap-4">
-          <Button @click="resetGame" class="w-min">Restart Game</Button>
-          <Button @click="() => toggleTileStack()" class="w-min" variant="ghost">Toggle Tile Stack</Button>
+        <div v-if="players.length" class="flex gap-3 items-center p-3 font-medium bg-neutral-100 rounded-md">
+          <SquareAsteriskIcon class="size-5" />
+          <p>{{ stack.length }} tiles left.</p>
         </div>
-        <div v-if="showTileStack" class="space-y-2">
+        <div v-if="!players.length" class="flex gap-3 items-center p-3 font-medium bg-neutral-100 rounded-md">
+          <InfoIcon class="size-5" />
+          <p>Click the notches at the edge of the board to add players.</p>
+        </div>
+        <div v-else class="flex gap-3 items-center p-3 font-medium bg-neutral-100 rounded-md">
+          <InfoIcon class="size-5" />
+          <p>Click <ArrowUpFromLineIcon class="size-4 inline" /> to play the tile. Click <RotateCcwIcon class="size-4 inline" /> to rotate the tile.</p>
+        </div>
+        <div class="flex gap-4">
+          <Button @click="resetGame" class="w-min gap-3">
+            <UndoIcon class="size-5" />
+            <span>Restart Game</span>
+          </Button>
+          <Button @click="() => toggleNerdStuff()" class="w-min gap-3" variant="ghost">
+            <EyeIcon class="size-5" />
+            <span>Toggle Tile Stack</span>
+          </Button>
+        </div>
+        <div v-if="showNerdStuff" class="space-y-2">
           <h3 class="font-medium">Tile Stack</h3>
           <TileStack :tiles="stack" />
         </div>
       </div>
     </div>
-    <p class="p-2 bg-neutral-200 rounded-md font-mono">{{ board }}</p>
-    <p class="p-2 bg-neutral-200 rounded-md font-mono">{{ stack }}</p>
-    <p class="p-2 bg-neutral-200 rounded-md font-mono">{{ players }}</p>
+    <p v-if="showNerdStuff" class="p-2 bg-neutral-200 rounded-md font-mono">{{ board }}</p>
+    <p v-if="showNerdStuff" class="p-2 bg-neutral-200 rounded-md font-mono">{{ stack }}</p>
+    <p v-if="showNerdStuff" class="p-2 bg-neutral-200 rounded-md font-mono">{{ players }}</p>
   </main>
 </template>
